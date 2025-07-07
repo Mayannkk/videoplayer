ac1bfe3419143ee775254de6d1288d236c88415a
Mayank Pal <mayank.hiranyawal1999@outlook.com>
2025-07-07 13:38:03 +0530

    connect to mongoDB atlas

---
+++ src/index.js
@@ -0,0 +1,34 @@
+// require('dotenv').config({ path: './env' });
+import dotenv from 'dotenv';
+import connectToDatabase from './db/index.js';
+
+dotenv.config({ path: './env' });
+
+connectToDatabase();
+
+/*
+    * This is the first approach to connect to MongoDB using Mongoose, directly in the index.js file.
+    * In this approach, we connect to MongoDB and start the server in the same file.
+    * This is a simple and straightforward way to set up the server and database connection.
+    * However, it is not the best practice for larger applications.
+    
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 4000;
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on('error', (error) => {
+      console.log('ERR: ', error);
+      throw error;
+    });
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+})();
+*/
